dc23e8f8cc33b2488ea496d679e6f328d23e4f7b
Brian Tobal <brian.tobal@gmail.com>
2019-04-24 11:16:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,10 @@
-class Person(hash)
+class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-
-  def initialize(attributes)     
-    if attributes 
+  def initialize(attributes=nil)
+    if attributes
       attributes.each do |k,v|
         self.send("#{k}=", v)
-      end 
-    end 
-  end     #def 
-
+      end
+    end
+  end
 end
